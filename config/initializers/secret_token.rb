e49fe7c3c1f7523af566f463cf0df82cd925a23c
Sean Broderick <hakutsuru@mac.com>
2012-09-04 23:21:52 -0400

    Initialize Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AgileWebDevDepot::Application.config.secret_token = '3e756b30572f7b538687e2ff9518190bd046f4ad54c6c56ca472c4c70ba693174787d222cd519fe3a76fab0c880951746b49d65eea63d467bf403e9979dc23c3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AgileWebDevDepot::Application.config.session_store :cookie_store, :key => '_agile_web_dev_depot_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AgileWebDevDepot::Application.config.session_store :active_record_store
