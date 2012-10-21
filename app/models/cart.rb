2e5cf2054ca84f5a24a1f7a949fcc9c925c72b39
Sean Broderick <hakutsuru@mac.com>
2012-10-21 18:53:39 -0400

    Iteration F4

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -12,6 +12,10 @@ class Cart < ActiveRecord::Base
     current_item
   end
 
+  def total_items
+    line_items.sum(:quantity)
+  end
+
   def total_price
     line_items.to_a.sum { |item| item.total_price }
   end
